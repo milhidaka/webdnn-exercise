7bc289e43cbf317b80f229fcdfaea70f67cf61b0
Kikurage <y.kikura@gmail.com>
2017-11-07 19:11:08 +0900

    improve paint logic

--- exercise/paint.js
+++ exercise/paint.js
@@ -1,76 +1,133 @@
 'use strict';
 
-// Simple paiting feature for canvas
+/////////////////////////////////////////////////////////////////////////
+//
+// paint.js
+//
+// 画面に文字を描くためのプログラムです。編集する必要はありません
+// Simple paiting feature for canvas. You DO NOT need to edit this file.
+//
+/////////////////////////////////////////////////////////////////////////
 
-window.addEventListener('load', function () {
-  var canvas_draw = document.getElementById('draw');
-  var canvas_draw_ctx = canvas_draw.getContext('2d');
-  // mouse drawing related variables
-  var draw_mouse_down = false;
-  var point_radius = 20;
 
-  // mouse drawing
-  function draw_point(x, y) {
-    canvas_draw_ctx.fillStyle = 'white';
-    canvas_draw_ctx.beginPath();
-    canvas_draw_ctx.arc(x, y, point_radius, 0, Math.PI * 2, false);
-    canvas_draw_ctx.fill();
+class SimpleCanvasPainter {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.context = canvas.getContext('2d');
+    this.lastPoint = {
+      x: 0,
+      y: 0
+    };
+    this.nextPoint = {
+      x: 0,
+      y: 0
+    };
+    this.flagContinueStroke = false;
+    this.flagStrokeRequested = false;
+    this.flagCalculationRequested = false;
+
+    this.canvas.style.touchAction = 'none';
+    this.context.fillStyle = 'black';
+    this.context.strokeStyle = 'white';
+    this.context.lineCap = 'round';
+    this.context.lineWidth = '10';
+
+    canvas.addEventListener('mousedown', (ev) => this.onMouseDown(ev));
+    canvas.addEventListener('mousemove', (ev) => this.onMouseMove(ev));
+    canvas.addEventListener('mouseup', (ev) => this.onMouseUp(ev));
+    canvas.addEventListener('touchstart', (ev) => this.onTouchStart(ev));
+    canvas.addEventListener('touchmove', (ev) => this.onTouchMove(ev));
+    canvas.addEventListener('touchend', (ev) => this.onTouchEnd(ev));
+    this.clear();
   }
 
-  function draw_clear() {
-    // clearRect fills with TRANSPARENT black; not suitable
-    canvas_draw_ctx.fillStyle = 'black';
-    canvas_draw_ctx.fillRect(0, 0, canvas_draw.width, canvas_draw.height);
+  clear() {
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
-  var update_timer_id = null;
-  var dnn_calculating = false;
-  function reserve_update() {
-    if (update_timer_id) {
-      clearTimeout(update_timer_id);
-    }
-    update_timer_id = setTimeout(function () {
-      update_timer_id = null;
-      if (!dnn_calculating) {
-        dnn_calculating = true;
-        calculate().then(() => {
-          dnn_calculating = false;
-        }).catch((reason) => {
-          console.error('Failed DNN calculation!', reason);
-        });
-      } else {
-        // DNN is now calculating, so avoid another run
-      }
-    }, 100);
+  startStroke(x, y) {
+    if (this.flagContinueStroke) return;
+    this.flagContinueStroke = true;
+    this.lastPoint.x = this.nextPoint.x = x;
+    this.lastPoint.y = this.nextPoint.y = y;
   }
 
-  draw_clear();
+  requestStrokeTo(x, y) {
+    if (!this.flagContinueStroke) return;
+    this.nextPoint.x = x;
+    this.nextPoint.y = y;
 
-  // mouse drawing events
-  canvas_draw.addEventListener('mousedown', function (e) {
-    draw_mouse_down = true;
-    var rect = e.target.getBoundingClientRect();
-    var x = e.clientX - rect.left;
-    var y = e.clientY - rect.top;
-    draw_point(x, y);
-    reserve_update();
-  });
-  canvas_draw.addEventListener('mousemove', function (e) {
-    if (draw_mouse_down) {
-      var rect = e.target.getBoundingClientRect();
-      var x = e.clientX - rect.left;
-      var y = e.clientY - rect.top;
-      draw_point(x, y);
-      reserve_update();
-    }
-  });
-  canvas_draw.addEventListener('mouseup', function (e) {
-    draw_mouse_down = false;
-  });
+    if (this.flagStrokeRequested) return;
+    this.flagStrokeRequested = true;
 
-  document.getElementById('reset').addEventListener('click', function (e) {
-    draw_clear();
-    reserve_update();
-  });
+    requestAnimationFrame(() => {
+      this.context.beginPath();
+      this.context.moveTo(this.lastPoint.x, this.lastPoint.y);
+      this.context.lineTo(this.nextPoint.x, this.nextPoint.y);
+      this.context.stroke();
+      this.lastPoint = this.nextPoint;
+      this.nextPoint = {
+        x: 0,
+        y: 0
+      };
+      this.flagStrokeRequested = false;
+      this.requestCalculation();
+    });
+  }
 
+  finishStroke() {
+    if (!this.flagContinueStroke) return;
+    this.flagContinueStroke = false;
+  }
+
+  requestCalculation() {
+    if (this.flagCalculationRequested) return;
+    this.flagCalculationRequested = true;
+
+    calculate()
+      .then(() => {
+        this.flagCalculationRequested = false;
+      })
+      .catch((reason) => {
+        console.error('Failed DNN calculation!', reason);
+      });
+  }
+
+
+  /**
+   * Event Handlers
+   */
+
+  onMouseDown(ev) {
+    this.startStroke(ev.offsetX, ev.offsetY);
+  }
+
+  onMouseMove(ev) {
+    if (!this.flagContinueStroke) return;
+    this.requestStrokeTo(ev.offsetX, ev.offsetY);
+  }
+
+  onMouseUp(ev) {
+    this.finishStroke();
+  }
+
+  onTouchStart(ev) {
+    let bcr = this.canvas.getBoundingClientRect();
+    this.startStroke(ev.touches[0].clientX - bcr.left, ev.touches[0].clientY - bcr.top);
+  }
+
+  onTouchMove(ev) {
+    if (!this.flagContinueStroke) return;
+    let bcr = this.canvas.getBoundingClientRect();
+    this.requestStrokeTo(ev.touches[0].clientX - bcr.left, ev.touches[0].clientY - bcr.top);
+  }
+
+  onTouchEnd(ev) {
+    this.finishStroke();
+  }
+}
+
+window.addEventListener('load', () => {
+  let painter = new SimpleCanvasPainter(document.getElementById('draw'));
+  document.getElementById('reset').addEventListener('click', (e) => painter.clear());
 });
