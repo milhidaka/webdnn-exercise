969d9ca4cf00315719c1d0b523848e4d40db5593
Masatoshi Hidaka <hidaka@mi.t.u-tokyo.ac.jp>
2017-11-07 15:24:55 +0900

    add comments

--- answer/convert_model_chainer.py
+++ answer/convert_model_chainer.py
@@ -12,15 +12,20 @@ from train_mnist_chainer import CNN  # import model definition
 
 
 def main():
+    # construct model object and load weights
     model = chainer.links.Classifier(CNN())
     chainer.serializers.load_npz('chainer_output/chainer_model.npz', model)
-    predictor = model.predictor
+
+    # run model with dummy variable
     input_variable = chainer.Variable(np.zeros((1, 1, 28, 28), dtype=np.float32))
-    prediction_raw_variable = predictor(input_variable)
+    prediction_raw_variable = model.predictor(input_variable)  # raw activation before softmax
     prediction_with_softmax_variable = chainer.functions.softmax(prediction_raw_variable)
 
+    # convert graph to intermediate representation
     graph = ChainerConverter().convert([input_variable], [
-        prediction_with_softmax_variable])  # convert graph to intermediate representation
+        prediction_with_softmax_variable])
+
+    # generate graph descriptor
     backend = 'webgl'
     exec_info = generate_descriptor(backend, graph)
     exec_info.save('webdnn_graph_descriptor')
