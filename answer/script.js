fd111ad758e0564dc51adfe56462e9f8dae9e03d
Masatoshi Hidaka <hidaka@mi.t.u-tokyo.ac.jp>
2017-11-07 17:09:15 +0900

    add comment

--- answer/script.js
+++ answer/script.js
@@ -8,7 +8,7 @@ async function initialize() {
   console.log('Beginning of initialize()');
   // Load WebDNN model
   webdnn_runner = await WebDNN.load('./webdnn_graph_descriptor');
-  // Get reference for input / output variable of the model
+  // Get view object for input / output variable of the model
   // There can be multiple variables for input / output, but there is only one for this model
   webdnn_input_view = webdnn_runner.getInputViews()[0];
   webdnn_output_view = webdnn_runner.getOutputViews()[0];
@@ -23,6 +23,7 @@ async function calculate() {
   // https://mil-tokyo.github.io/webdnn/docs/api_reference/descriptor-runner/modules/webdnn_image.html
   var canvas_draw = document.getElementById('draw'); // canvas object that contains input image
   webdnn_input_view.set(await WebDNN.Image.getImageArray(canvas_draw, {
+    /* convert image on canvas into input array (Float32Array, 28px*28px*1ch=784 dimension, black=0.0 and white=1.0) */
     dstH: 28, dstW: 28,
     order: WebDNN.Image.Order.HWC, // for Keras
     // order: WebDNN.Image.Order.CHW, // for Chainer (but the same as HWC because channel=1 in MNIST)
@@ -34,7 +35,7 @@ async function calculate() {
   // run the DNN
   await webdnn_runner.run();
 
-  // get the result
+  // get model output as Float32Array
   var probabilities = webdnn_output_view.toActual();
   // 'probabilities' is array containing each label's probability (0.0 to 1.0)
   // display the result
